d852e5c992e6d2f36bce7d83a17d1465175dbab7
utkusama0 <utku.dursunoglu@sabanciuniv.edu>
2025-05-21 00:00:19 +0300

    update for frontend connection

--- frontend/student-management-ui/vite.config.js
+++ frontend/student-management-ui/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
@@ -21,6 +22,13 @@ export default defineConfig({
   server: {
     port: 3000,
     strictPort: true,
-    host: true
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:80',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 })
